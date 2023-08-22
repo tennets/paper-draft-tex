fbd334172889a789c58ec8cf384de20c06f9866b
Stephan Gahima <stephan.gahima@upc.edu>
2023-08-22 18:07:06 +0200

    Simplify functionalities. Remove early optimizations. Add input parsers and filename check.

--- template/src/matlab/ffsp.m
+++ template/src/matlab/ffsp.m
@@ -1,4 +1,4 @@
-function ffsp(varargin)
+function ffsp(f, ax, varargin)
 %FFSP formats Matlab-generated figures to produce high-quality LaTeX-ready
 %figures.
 %
@@ -12,13 +12,6 @@ function ffsp(varargin)
 %
 %   FFSP(..., FILENAME, FORMAT) format the figure to have FORMAT extension,
 %   and saves it as FORMATTED_FIGURE.FORMAT. 
-%
-%   FFSP(..., TRANSPARENT) format the figure to have transparent
-%   background. All the previous options are valid.
-%
-%   FFSP(VERSION) print the version of this software.
-%
-%   FFSP(HELP) print this help message.
 %
 %   INPUTS:
 %   F                    To be formatted figure handle. See GCF (built-in).
@@ -37,12 +30,6 @@ function ffsp(varargin)
 %                        '-eps'   Encapsulated PostScript (EPS) format.
 %                        '-pdf'   Portable Document Format (PDF).
 %
-%   TRANSPARENT          Set the figure background to be transparent.
-%
-%   -v, -V, --version    Print current version.
-%
-%   -h, -H, --help       Print help message.
-%
 %   NOTE
 %   To arrange figures for a LaTeX document using FFSP, use the SUBFIGURE 
 %   within the FIGURE environment to create an N-by-M grid format.
@@ -56,75 +43,87 @@ function ffsp(varargin)
 %
 % Inspired by https://github.com/altmany/export_fig and https://github.com/plotly/plotly_matlab.
 
+% DO NOT MODIFY -----------------------------------------------------------
 % Software information
 CURRENT_VERSION = 0.1;
 VERSION_FORMAT  = "%.1f";
 % Use CHAR to display nicely on the command window
 RELEASE_DATE    = char(datetime("today", "Format", "d-MM-y"));
+% Default values
+DEFAULT_FILENAME  = "formatted_figure";
+DEFAULT_FORMAT    = "-pdf";
+AVAILABLE_FORMATS = ["-eps", "-pdf"];
+% -------------------------------------------------------------------------
 
-% PARSING ---
-opts = parse_args(nargin);
-
-if opts.version
-    disp(['ffsp version ', num2str(CURRENT_VERSION, VERSION_FORMAT), ...
+disp(['ffsp version ', num2str(CURRENT_VERSION, VERSION_FORMAT), ...
         ' (', RELEASE_DATE, ')'])
-    return
-end
 
-if opts.help
-    disp('usage : ffsp <f> <ax> [filename] [format] [transparent]   ')
-    disp('                      [-h, -H, --help] [-v, -V, --version]')
-    return
-end
+p = parse_args();
 
+% Unpack 
+f_handle  = p.Results.f;
+ax_handle = p.Results.ax;
+filename  = p.Results.filename;
+% format    = p.format;
+
+check_filename();
+ 
 % MAIN ---
-
 
 % SAVE ---
 
 % LOCAL FUNCTIONS ---------------------------------------------------------
 
-function opts = parse_args(n)
+function p = parse_args()
 %PARSE_ARGS parse the input arguments.
 
-    VERSION_TOKENS    = {"-v", "--version"};
-    HELP_TOKENS       = {"-h", "--version"};
-    AVAILABLE_FORMATS = {"-eps", "-pdf"};
-    HANDLES           = {"figure", "axes"};
-    DEFAULT_FILENAME  = "formatted_figure";
-    DEFAULT_FORMAT    = "-pdf";
-    DEFAULT_PATH      = "./";
+    % Helper functions
+    check_f  = @(x) isgraphics(f, 'figure') && isa(f, 'matlab.ui.Figure');
+    check_ax = @(x) isgraphics(ax, 'axes')  && isa(ax, 'matlab.graphics.axis.Axes');
+    check_op = @(x) isstring(x) | ischar(x);
+
+    % Create parsing object
+    p = inputParser;
+    % Set function name
+    p.FunctionName = 'ffsp';
+
+    % Check f (current figure handle) 
+    addRequired(p, 'f', check_f);
+    % Check ax (current axes) 
+    addRequired(p, 'ax', check_ax);
+
+    addParameter(p, 'filename', DEFAULT_FILENAME, check_op);
+    addParameter(p, 'format',  DEFAULT_FORMAT, @(x) any(validatestring(x, AVAILABLE_FORMATS)));
+
+    parse(p, f, ax, varargin{:});
     
-    % Initialize options
-    opts.fig_handle  = false;
-    opts.ax_handle   = false;
-    opts.transparent = false;
-    opts.version     = false;
-    opts.help        = false;
-    opts.format      = DEFAULT_FORMAT;
-    opts.filename    = DEFAULT_FILENAME;
-    opts.path        = DEFAULT_PATH;
+end
 
-    % Loop over the inputs
-    for i = 1:n
+% -------------------------------------------------------------------------
 
-        arg = varargin{i};
+function check_filename(INVALID_CHARACTERS)
+%CHECK_FILENAME ensures filename is valid on different OS.
 
-        if strcmpi(arg, VERSION_TOKENS{1}) || strcmpi(arg, VERSION_TOKENS{2})
-            opts.version = true;
-        elseif strcmpi(arg, HELP_TOKENS{1}) || strcmpi(arg, HELP_TOKENS{2})
-            opts.help = true;
-        elseif strcmpi(arg, AVAILABLE_FORMATS{1}) || strcmpi(arg, AVAILABLE_FORMATS{2})
-            opts.format = arg;
-        elseif strcmpi(arg, "transparent")
-            opts.transparent = true;
-        elseif strcmp(get(arg, 'type'), HANDLES{1})
-            opts.fig_handle = true;
-        elseif strcmp(get(arg, 'type'), HANDLES{2})
-            opts.ax_handle = true;
-        end
+% Ref. https://stackoverflow.com/questions/4814040/allowed-characters-in-filename
+if ismac
+    INVALID_CHARACTERS = [':', '/'];
+elseif ispc
+    INVALID_CHARACTERS = ["NUL", '\', '/', ':', '*', '?', '"', '<', '>', '|'];
+elseif isunix
+    INVALID_CHARACTERS = ["NUL", '/'];
+else
+    % Throw a warning for an unknown platform
+    warning('FFSP:UnknownPlatform', ...
+        ['Cannot check if "', filename, '" is a valid/legal filename.'])
+end
 
+for i = 1:length(INVALID_CHARACTERS)
+    c = INVALID_CHARACTERS(i);
+    if contains(filename, c)
+        error('FFSP:CheckFilename', [c, ' is an invalid character for ' ...
+            'filename on your OS.']);
     end
+end
 
 end
 

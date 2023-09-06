d2b407af760f8d1b5f6e9359add4609b1d491342
Stephan Gahima <stephan.gahima@upc.edu>
2023-09-06 15:56:28 +0200

    Update function description, fix bug in parsing input, and update logic

--- matlab/ffsp.m
+++ matlab/ffsp.m
@@ -1,192 +1,232 @@
 function ffsp(f, ax, LATEXW, LATEXH, NROWS, NCOLS, varargin)
-% FFSP formats Matlab-generated figures to be high-quality LaTeX-ready 
-% figures.
-%
+%FFSP formats Matlab-generated figures to be high-quality LaTeX-ready 
+%figures.
+% 
 %   USAGE:
 %   FFSP(f, ax, LATEXW, LATEXH, NROWS, NCOLS) saves the figure using 
-%   default settings as FORMATTED_FIGURE.pdf.
-%
-%   FFSP(..., FILENAME) saves the figure using the default extension as 
-%   FILENAME.pdf.
-%
-%   FFSP(..., FORMAT) saves the figure using the default filename as 
-%   FORMATTED_FIGURE.FORMAT. 
-%
-%   FFSP(..., FILENAME, FORMAT) saves the figure as FILENAME.FORMAT.
-%
+%   default settings as FORMATTED_FIGURE.pdf
+% 
+%   FFSP(___, "FILENAME", "figname") saves the figure using the default 
+%   extension as FILENAME.pdf
+% 
+%   FFSP(___, "FORMAT", "figext") saves the figure using the default
+%   filename as FORMATTED_FIGURE.figext 
+% 
+%   FFSP(___, "FILENAME", "figname", "FORMAT", "figext") saves the figure 
+%   as figname.figext
+% 
+%   Since R2021a, it is possible to specify Name-Value Arguments as
+%   FFSP(___, FILENAME="figname")
+%   FFSP(___, FORMAT="figext")
+%   FFSP(___, FILENAME="figname", FORMAT="figext")
+% 
 %   INPUTS:
+%   Required Arguments
+%   -----------------------------------------------------------------------
 %   F                    The figure handle of the target graphic object.
 %                        See GCF (built-in).
-%
-%   AX                   The axes handle of the target graphic object.
-%                        See GCA (built-in).
-%
+% 
+%   AX                   The axes handle of the target graphic object. See 
+%                        GCA (built-in).
+% 
 %   LATEXW               The \textwidth (in cm) of the target LaTex 
 %                        document.
-%
+% 
 %   LATEXH               The \textheigh (in cm) of the target LaTex
 %                        document.
-%
+% 
 %                        The easiest way to get those values is to print
 %                        them on your LaTex document. To do so, write 
 %                        \printinunitsof{cm}\prntlen{\textwidth} and
-%                        \printinunitsof{cm}\prntlen{\textheight} to 
-%                        print LATEXW and LATEXH respectively. 
-%
+%                        \printinunitsof{cm}\prntlen{\textheight} to print
+%                        LATEXW and LATEXH respectively. 
+% 
 %   NROWS                Number of grid rows in the layout. 
-%
+% 
 %   NCOLS                Number of grid columns in the layout.
-%
+% 
 %                        FFSP assumes that the target figure is arranged
 %                        in the LaTex document using the SUBFIGURE 
 %                        environment within the FIGURE environment. In 
 %                        particular, FFSP assumes an NROW-by-NCOLS grid 
 %                        layout of SUBFIGURES.  
-%
-%   FILENAME             String with the desired output filename. FFSP 
+% 
+%   Name-Value Arguments
+%   -----------------------------------------------------------------------
+%   FILENAME             String with the output figure filename. FFSP 
 %                        uses the default name "formatted_figure" if 
-%                        FILENAME is empty.
-%
-%   FORMAT               String containing the output file extension. 
+%                        FILENAME is empty.  
+% 
+%   FORMAT               String with the output figure extension. 
 %                        Specify one of the following options:
 %                        'eps'   Encapsulated PostScript (EPS) format.
 %                        'pdf'   Portable Document Format (PDF).
-%
+%                        FFSP uses the default extension "pdf" if FORMAT is
+%                        empty.
+% 
 %   NOTE
 %   If you format figures for a LaTeX document using FFSP, use the 
 %   SUBFIGURE environment within the FIGURE environment to create an
 %   NROWS-by-NCOLS grid layout. Check the recommended LaTeX template at
-%   https://github.com/tennets/paper-draft-tex for more details about 
-%   the inner workings of FFSP, together with some examples.
-%
+%   https://github.com/tennets/paper-draft-tex for more details about the 
+%   inner workings of FFSP, together with some examples.
+% 
 %   LIMITATIONS
 %   - FFSP assumes NROWS and NCOLS range from 1 to 4.
 %   - FFSP save the figure in the directory where FFSP runs.
 %   - FFSP only supports PDF and EPS formats.
-%
-% ffsp.m
-% Produce Matlab-generated high-quality LaTex-ready figures
-% Stephan Gahima/LaTex/Matlab
-%
-% Inspired by https://github.com/altmany/export_fig, 
-% https://github.com/plotly/plotly_matlab, aesthetics and desire to 
-% write a (simpler and basic) version of what's out there.
-%
+%   - FFSP returns a grey scale figure when EPS is specified.
+% 
+%   ffsp.m
+%   Produce Matlab-generated high-quality LaTex-ready figures
+%   Stephan Gahima/LaTex/Matlab
+% 
+%   Inspired by https://github.com/altmany/export_fig, 
+%   https://github.com/plotly/plotly_matlab, aesthetics and desire to write
+%   a (simpler and basic) version of what's out there.
+% 
 % Copyright (c) 2023 Stephan Gahima.
 
 % ------------------------ DO NOT EDIT ------------------------------------
 % Software information
-CURRENT_VERSION = 0.7;
+CURRENT_VERSION = 0.9;
 VERSION_FORMAT  = "%.1f";
 % Use CHAR to display nicely on the command window
 RELEASE_DATE    = char(datetime("today", "Format", "d-MM-y"));
-% Default values
-DEFAULT_FILENAME  = "formatted_figure";
-DEFAULT_FORMAT    = "pdf";
-AVAILABLE_FORMATS = ["eps", "pdf"];
 % -------------------------------------------------------------------------
 
 disp(['ffsp version ', num2str(CURRENT_VERSION, VERSION_FORMAT), ...
         ' (', RELEASE_DATE, ')'])
 
-% Unpack 
-filename = parse_args().Results.filename;
-format   = parse_args().Results.format;
-% LATEXW   = parse_args().Results.LATEXW;
-% LATEXH   = parse_args().Results.LATEXH;
-% NROWS    = parse_args().Results.NROWS;
-% NCOLS    = parse_args().Results.NCOLS;
+args = parseArgs();
+
+% Unpack arguments
+filename = args.Results.filename;
+format   = args.Results.format;
+LATEXW   = args.Results.LATEXW;
+LATEXH   = args.Results.LATEXH;
+NROWS    = args.Results.NROWS;
+NCOLS    = args.Results.NCOLS;
 
 check_filename();
  
 % MAIN ---
 
-% Work in centimeters
-set(ax, 'Units', 'centimeters'); 
-set(f, 'PaperUnits', 'centimeters', 'Units', 'centimeters');
+% Working with centimeters
+ul = "centimeters";
 
-ax_pos = get(ax, 'Position');
-inset  = get(ax, 'TightInset');
+% Set unit of length
+set(ax, "Units", ul); 
+set(f, "PaperUnits", ul, "Units", ul);
+% Get axes information
+ax_pos = get(ax, "Position");
+inset  = get(ax, "TightInset");
 
 % Original width and height
-ow = sum(ax_pos([1, 3]))+inset(3);
-oh = sum(ax_pos([2, 4]))+inset(4);
+ow = sum(ax_pos([1, 3])) + inset(3);
+oh = sum(ax_pos([2, 4])) + inset(4);
 
 % Based on NROWS and NCOLS, scale the original width and height.
 % Assume that the NROWS-by-NCOLS figures must fit on a LaTex page.
-
 subh = subfig_scale_factor(NROWS);
 subw = subfig_scale_factor(NCOLS);
-   
+
+% The strategy aims to ensure the grid layout. First, we try to scale
+% based on a preferred dimension (say width) and adapt the second one 
+% (height) accordingly. If the height is such that the layout is not 
+% respected, we try the other way, that is, we scale the other dimension
+% (height) and adapt the width accordingly.   
 if NROWS > NCOLS
 
-    height = LATEXW * subh;
-    scale  = oh / height;
-    width  = ow / scale;
-    % #TODO Make sure we fit in LATEXW 
+    [width, height] = scale_height();
+
+    if width >= LATEXW * subw
+        [width, height] = scale_width();
+    end
 
 elseif NROWS < NCOLS
 
-    width  = LATEXH * subw;
-    scale  = ow / width;
-    height = oh / scale;
+    [width, height] = scale_width();
 
-    % #TODO Make sure we fit in LATEXH
+    if height >= LATEXH * subh
+        [width, height] = scale_height();
+    end
 
 else 
 
-    % #TODO Be more clever
-    width  = LATEXH * subw;
-    scale  = ow / width;
-    height = oh / scale;
+    % #TODO Maybe we can be more clever    
+    [width, height] = scale_width();
 
-    % #TODO Make sure we fit in LATEXH and LATEXW
+    if height >= LATEXH * subh
+        [width, height] = scale_height();
+    end
 
 end
 
-% Set font family and font size
+% #TODO Fix linestiles for EPS images
 
+% #TODO Set font family and font size
 
 % Ref. https://undocumentedmatlab.com/articles/axes-looseinset-property/
-set(ax, 'LooseInset', [0, 0, 0, 0]); % inset); % Not sure which yields the best results
+set(ax, "LooseInset", [0, 0, 0, 0]); % inset); % Not sure which yields the best results
 
 set(f,                                ...
-    'PaperPositionMode', 'manual',    ... 
-    'PaperSize', [width, height],     ...
-    'PaperPosition', [0, 0,           ...
+    "PaperPositionMode", "manual",    ... 
+    "PaperSize", [width, height],     ...
+    "PaperPosition", [0, 0,           ...
                       width, height], ...
-    'Renderer', 'painters');
-
-% #TODO Fix linestiles for EPS images
+    "Renderer", "painters");
 
 % SAVE ---
 print(f, filename, strcat("-d", format));
 
 % LOCAL FUNCTIONS ---------------------------------------------------------
 
-function p = parse_args()
-%PARSE_ARGS parse the input arguments.
-
-    % Helper functions
-    check_f  = @(x) isgraphics(f, 'figure') && isa(f, 'matlab.ui.Figure');
-    check_ax = @(x) isgraphics(ax, 'axes')  && isa(ax, 'matlab.graphics.axis.Axes');
-    check_op = @(x) isstring(x) | ischar(x);
-    % #TODO add checks for LATEXW, LATEXH, NROWS, and NCOLS
+function p = parseArgs()
+%PARSEARGS parse the input arguments.
 
     % Create parsing object
     p = inputParser;
     % Set function name
-    p.FunctionName = 'ffsp';
+    p.FunctionName = "ffsp";
 
+    % Default values ---
+    DEFAULT_FILENAME  = "formatted_figure";
+    DEFAULT_FORMAT    = "pdf";
+    AVAILABLE_FORMATS = ["eps", "pdf"];
+
+    % Helper functions ---
+    check_figure = @(x) isgraphics(f, "figure") && ...
+        isa(f, "matlab.ui.Figure");
+    check_axes   = @(x) isgraphics(ax, "axes")  && ...
+        isa(ax, "matlab.graphics.axis.Axes");
+    % Refuse inputs (in cm) greater than the height of a 4A0 paper.
+    % Ref. https://www.papersizes.org/a-paper-sizes.htm
+    check_latex  = @(x) isscalar(x) && x > 0 && x < 237.801; 
+    % NROWS and NCOLS range from 1 to 4
+    check_grid   = @(x) isscalar(x) && ~mod(x, 1) && x > 0 && x < 5;
+    check_name   = @(x) isstring(x) | ischar(x);
+    check_format = @(x) strlength(validatestring(x, AVAILABLE_FORMATS));
+
+    % REQUIRED ARGUMENTS ---
     % Check f (current figure handle) 
-    addRequired(p, 'f', check_f);
+    addRequired(p, "f", check_figure);
     % Check ax (current axes) 
-    addRequired(p, 'ax', check_ax);
+    addRequired(p, "ax", check_axes);
+    % Check width
+    addRequired(p, "LATEXW", check_latex);
+    % Check height
+    addRequired(p, "LATEXH", check_latex);
+    % Check number of rows
+    addRequired(p, "NROWS", check_grid);
+    % Check number of columns
+    addRequired(p, "NCOLS", check_grid);
 
-    addParameter(p, 'filename', DEFAULT_FILENAME, check_op);
-    addParameter(p, 'format',  DEFAULT_FORMAT, @(x) any(validatestring(x, AVAILABLE_FORMATS)));
+    % NAME-VALUE ARGUMENTS ---
+    addParameter(p, "filename", DEFAULT_FILENAME, check_name);
+    addParameter(p, "format", DEFAULT_FORMAT, check_format);
 
-    parse(p, f, ax, varargin{:});
+    parse(p, f, ax, LATEXW, LATEXH, NROWS, NCOLS, varargin{:});
     
 end
 
@@ -195,46 +235,71 @@ end
 function check_filename()
 %CHECK_FILENAME ensures filename is valid on different OS.
 
-% Ref. https://stackoverflow.com/questions/4814040/allowed-characters-in-filename
-if ismac
-    INVALID_CHARACTERS = [':', '/'];
-elseif ispc
-    INVALID_CHARACTERS = ["NUL", '\', '/', ':', '*', '?', '"', '<', '>', '|'];
-elseif isunix
-    INVALID_CHARACTERS = ["NUL", '/'];
-else
-    % Throw a warning for an unknown platform
-    warning('FFSP:UnknownPlatform', ...
-        ['Cannot check if "', filename, '" is a valid/legal filename.'])
-end
-
-for i = 1:length(INVALID_CHARACTERS)
-    c = INVALID_CHARACTERS(i);
-    if contains(filename, c)
-        error('FFSP:CheckFilename', [c, ' is an invalid character for ' ...
-            'filename on your OS.']);
+    % Ref. https://stackoverflow.com/questions/4814040/allowed-characters-in-filename
+    if ismac
+        INVALID_CHARACTERS = [':', '/'];
+    elseif ispc
+        INVALID_CHARACTERS = ["NUL", '\', '/', ':', '*', '?', '"', '<', '>', '|'];
+    elseif isunix
+        INVALID_CHARACTERS = ["NUL", '/'];
+    else
+        % Throw a warning for an unknown platform
+        warning("FFSP:UnknownPlatform", ...
+            ['Cannot check if "', filename, '" is a valid/legal filename.'])
     end
-end
+    
+    for i = 1:length(INVALID_CHARACTERS)
+        c = INVALID_CHARACTERS(i);
+        if contains(filename, c)
+            error("FFSP:CheckFilename", [c, ...
+                ' is an invalid character for filename on your OS.']);
+        end
+    end
 
 end
 
 % -------------------------------------------------------------------------
 
 function subl = subfig_scale_factor(N)
+%SUBFIG_SCALE_FACTOR returns a scale factor used to scale the subfigures.
+%
+%   These values are defined at lines 69-72 in main.tex in the latex 
+%   template you find at https://github.com/tennets/paper-draft-tex.
 
-if N == 1
-    subl = .9;
-elseif N == 2
-    subl = .425;
-elseif N == 3
-    subl = .285;
-elseif N == 4
-    subl = .2125;
-else
-    % #TODO Decide what to do for large values
+    if N == 1
+        subl = .9;
+    elseif N == 2
+        subl = .45;
+    elseif N == 3
+        subl = .3;
+    else 
+        subl = .225; 
+    end
+
 end
 
+% -------------------------------------------------------------------------
+
+function [width, height] = scale_height()
+%SCALE_HEIGHT returns the figure dimensions by height-scaling first.
+
+    height = LATEXH * subh;
+    scale  = oh / height;
+    width  = ow / scale;
+
 end
+
+% -------------------------------------------------------------------------
+
+function [width, height] = scale_width()
+%SCALE_WIDTH returns the figure dimensions by width-scaling first.
+
+    width  = LATEXW * subw;
+    scale  = ow / width;
+    height = oh / scale;
+
+end
+
 
 % LICENSE -----------------------------------------------------------------
 % MIT License
